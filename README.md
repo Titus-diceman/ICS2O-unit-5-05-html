84609de70fc728dc6eeb983a736a6075e1fc9397
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-05-05 16:40:23 +0000

    changed code

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# ICS20-PWA-Template
+# ICS2O-unit-5-05-html
 
-[![GitHub's Super Linter](https://github.com/Curtis-Edwards/ICS20-PWA-Template/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Curtis-Edwards/ICS20-PWA-Template/actions)
+[![GitHub's Super Linter](https://github.com/Titus-diceman/ICS2O-unit-5-05-html/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Titus-diceman/ICS2O-unit-5-05-html/actions)
 
-[![Run on Repl.it](https://repl.it/badge/github/Curtis-Edwards/ICS20-PWA-Template)](https://repl.it/github/Curtis-Edwards/ICS20-PWA-Template)
+[![Run on Repl.it](https://repl.it/badge/github/Titus-diceman/ICS2O-unit-5-05-html)](https://repl.it/github/Titus-diceman/ICS2O-unit-5-05-html)
 
-This site can be found at: [https://Curtis-Edwards.github.io/ICS20-PWA-Template](https://Curtis-Edwards.github.io/ICS20-PWA-Template)
+This site can be found at: [https://Titus-diceman.github.io/ICS2O-unit-5-05-html](https://Titus-diceman.github.io/ICS2O-unit-5-05-html)
--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-/* Created by: Titus Diceman
+/* Created by: Titus diceman
    Created on: April 2022
    This file contains the JS functions for index.html*/
 
 'use strict'
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
  if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-unit-5-05-html/sw.js", {
@@ -14,25 +14,33 @@
 }
 
 /**
- * This function displays the code for triangle types.
+ * Input.
  */
+
 function buttonClicked() {
+  
+  const sidea = parseInt(document.getElementById('side-a').value)
+  const sideb = parseInt(document.getElementById('side-b').value)
+  const sidec = parseInt(document.getElementById('side-c').value)
 
-  const aside = parseInt(documet.getElementById("aside").value)
-  const bside = parseInt(documet.getElementById("bside").value)
-  const cside = parseInt(documet.getElementById("cside").value)
 
-  if (aside == bside || aside == cside || bside == cside) {
-    document.getElemnentById("output").innerHTML
-    "this is a isosceles triangle" 
+  if (sidea == sideb && sideb == sidec && sidec == sidea) {
+    document.getElementById("output").innerHTML =
+  "Your triangle is an equilateral"
+  console.log("equilateral")
+  } else if (sidea == sideb || sideb == sidec || sidec == sidea) {
+    document.getElementById("output").innerHTML =
+  "Your triangle is an isosceles"
+  console.log("isosceles")
+  } else if (sidea != sideb && sideb != sidec && sidec != sidea) {
+    document.getElementById("output").innerHTML =
+  "Your triangle is an scalene"
+  console.log("scalene")
+  } else {
+  document.getElementById("output").innerHTML =
+  "Somthing went wrong..."
+  console.log("error")
   }
-  if (aside != bside && cside != aside && bside != cside) {
-    document.getElemnentById("output").innerHTML
-    "this is a scalene triangle" 
-  }
-  if (aside == bside && aside == cside && bside == cside) {
-    document.getElemnentById("output").innerHTML
-    "this is a equilateral triangle" 
-  }
-
+  
 }
+
