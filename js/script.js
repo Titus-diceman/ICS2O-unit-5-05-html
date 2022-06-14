69c612ad2d15505975217aa317ae126db4d528ae
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-06-14 00:37:10 +0000

    changed code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* Created by: Titus diceman
+/* Created by: Curtis Edwards
    Created on: April 2022
    This file contains the JS functions for index.html*/
 
@@ -8,8 +8,8 @@
  * Check service worker.
  */
  if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-unit-5-05-html/sw.js", {
-    scope: "/ICS2O-unit-5-05-html/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-5-05-HTML/sw.js", {
+    scope: "/ICS2O-UNIT-5-05-HTML/",
   })
 }
 
@@ -23,29 +23,31 @@ function buttonClicked() {
   const sideb = parseInt(document.getElementById('side-b').value)
   const sidec = parseInt(document.getElementById('side-c').value)
 
-  if (sidea <= 0 || sideb <= 0 || sidec <= 0) {
-    document.getElementById("output").innerHTML =
-      "you are wrong do it again but right this time"
+  if (sidea <= 0 || sideb <= 0 || sidec <= 0 ) {
+  document.getElementById("output").innerHTML =
+  "Somthing went wrong..."
+  console.log("error")
   } else {
-    
-  
+
     if (sidea == sideb && sideb == sidec && sidec == sidea) {
-      document.getElementById("output").innerHTML =
+    document.getElementById("output").innerHTML =
     "Your triangle is an equilateral"
     console.log("equilateral")
     } else if (sidea == sideb || sideb == sidec || sidec == sidea) {
-      document.getElementById("output").innerHTML =
+    document.getElementById("output").innerHTML =
     "Your triangle is an isosceles"
     console.log("isosceles")
     } else if (sidea != sideb && sideb != sidec && sidec != sidea) {
-      document.getElementById("output").innerHTML =
+    document.getElementById("output").innerHTML =
     "Your triangle is an scalene"
     console.log("scalene")
     } else {
     document.getElementById("output").innerHTML =
     "Somthing went wrong..."
     console.log("error")
-    }    
-  }
-}
 
+    }
+
+  }
+
+}
--- sw.js
+++ sw.js
@@ -9,21 +9,28 @@ var URLS = [
   `${GHPATH}/js/script.js`,
 ]
 
+var CACHE_NAME = APP_PREFIX + VERSION
+self.addEventListener("fetch", function (e) {
+  console.log("Fetch request : " + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        console.log("Responding with cache : " + e.request.url)
+        return request
+      } else {
+        console.log("File is not cached, fetching : " + e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
 self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       var cacheWhitelist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX)
       })
-      cacheWhitelist.push(CACHE_NAME)
-      return Promise.all(
-        keyList.map(function (key, i) {
-          if (cacheWhitelist.indexOf(key) === -1) {
-            console.log("Deleting cache : " + keyList[i])
-            return caches.delete(keyList[i])
-          }
-        })
-      )
     })
   )
 })
